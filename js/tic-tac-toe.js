2932adfcf94e9dc5e913bb3f288ef34d6577513a
hiagopsilva <hiagohps360@gmail.com>
2020-05-23 22:45:40 -0300

    update tic tac toe

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,13 +1,10 @@
-// TIC TAC TOE
 const tic_tac_toe = {
-
-    // ATTRIBUTES
     board: ['','','','','','','','',''],
     symbols: {
         options: ['O','X'],
         turn_index: 0,
         change: function(){
-            this.turn_index = ( this.turn_index === 0 ? 1:0 );
+            this.turn_index = ( this.turn_index === 0 ? 1 : 0 );
         }
     },
     container_element: null,
@@ -23,20 +20,20 @@ const tic_tac_toe = {
                         [2,4,6]
                     ],
 
-    // FUNCTIONS
     init: function(container) {
         this.container_element = container;
     },
 
     make_play: function(position) {
         if (this.gameover) return false;
+        
         if (this.board[position] === ''){
             this.board[position] = this.symbols.options[this.symbols.turn_index];
             this.draw();
             let winning_sequences_index = this.check_winning_sequences( this.symbols.options[this.symbols.turn_index] );
             if (winning_sequences_index >= 0){
                 this.game_is_over();
-                alert("o jogador 1 ganhou");
+                this.PlayerWinning();
             } else{
                 this.symbols.change();
             }
@@ -48,12 +45,10 @@ const tic_tac_toe = {
     },
 
     check_winning_sequences: function(simbol) {
-
         for ( i in this.winning_sequences ) {
             if (this.board[ this.winning_sequences[i][0] ] == simbol  &&
                 this.board[ this.winning_sequences[i][1] ] == simbol &&
                 this.board[ this.winning_sequences[i][2] ] == simbol) {
-                console.log('winning sequences INDEX:' + i);
                 return i;
             }
         };
@@ -62,7 +57,10 @@ const tic_tac_toe = {
 
     game_is_over: function() {
         this.gameover = true;
-        console.log('GAME OVER');
+
+        document.querySelector('.game').style.opacity = 0.2;
+        document.querySelector('.game').style.cursor = "default";
+        document.querySelector('.playAgain').style.display = "block";
     },
 
     start: function() {
@@ -73,11 +71,23 @@ const tic_tac_toe = {
 
     draw: function() {
         let content = '';
-
         for ( i in this.board ) {
             content += '<div onclick="tic_tac_toe.make_play(' + i + ')">' + this.board[i] + '</div>';
         };
 
         this.container_element.innerHTML = content;
     },
+
+    PlayerWinning: function() {
+        if(this.symbols.turn_index == 0) {
+            document.querySelector('.playerOneWin').style.display = "block";
+            
+        } else {
+            document.querySelector('.playerTwoWin').style.display = "block";
+        }
+    },
+
+    refreshWindow: function() {
+        Location.reload();
+    }
 };
